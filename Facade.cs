5b91620ff3c1b3b3a0f6a1116f9ecbd1faa356b2
Partha Pratim Bora <pamuba@users.noreply.github.com>
2026-02-18 10:28:34 +0530

    Add files via upload

---
+++ Facade.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using static DesignPatternDemos.Product;
+
+namespace DesignPatternDemos
+{
+    //public class Product
+    //{
+    //    public void GetProductDetails()
+    //    {
+    //        Console.WriteLine("Fetching the Product Details");
+    //    }
+    //    public class Payment
+    //    {
+    //        public void MakePayment()
+    //        {
+    //            Console.WriteLine("Payment Done Successfully");
+    //        }
+    //    }
+    //    public class Invoice
+    //    {
+    //        public void Sendinvoice()
+    //        {
+    //            Console.WriteLine("Invoice Send Successfully");
+    //        }
+    //    }
+    //    // The Facade class provides a simple interface to the complex logic of one
+    //    // or several subsystems. The Facade delegates the client requests to the
+    //    // appropriate objects within the subsystem. 
+    //    public class Order
+    //    {
+    //        public void PlaceOrder()
+    //        {
+    //            Console.WriteLine("Place Order Started");
+    //            //Get the Product Details
+    //            Product product = new Product();
+    //            product.GetProductDetails();
+    //            //Make the Payment
+    //            Payment payment = new Payment();
+    //            payment.MakePayment();
+    //            //Send the Invoice
+    //            Invoice invoice = new Invoice();
+    //            invoice.Sendinvoice();
+    //            Console.WriteLine("Order Placed Successfully");
+    //        }
+    //    }
+    //}
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        //The Client will use the Facade Interface instead of the Subsystems
+    //        Order order = new Order();
+    //        order.PlaceOrder();
+    //        Console.Read();
+    //    }
+    //}
+
+
+
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string MobileNumber { get; set; }
+        public string Address { get; set; }
+        //Any other Properties as per the Business Requirements
+    }
+    public class Validator
+    {
+        public bool ValidateCustomer(Customer customer)
+        {
+            //Need to Validate the Customer Object
+            Console.WriteLine("Customer Validated...");
+            Console.WriteLine($"Name:{customer.Name}");
+            Console.WriteLine($"Email:{customer.Email}");
+            Console.WriteLine($"Mobile:{customer.MobileNumber}");
+            Console.WriteLine($"Address:{customer.Address}");
+            return true;
+        }
+    }
+    public class CustomerDataAccessLayer
+    {
+        public bool SaveCustomer(Customer customer)
+        {
+            //Save the Customer in the Database
+            Console.WriteLine("\nCustomer Saved into the Database...");
+            return true;
+        }
+    }
+    public class Email
+    {
+        public bool SendRegistrationEmail(Customer customer)
+        {
+            //Send Registration Successful Email to Customer
+            Console.WriteLine("\nRegistration Email Send to Customer...");
+            return true;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Step1: Create an Instance of Customer Class
+            Customer customer = new Customer()
+            {
+                Name = "Pranaya",
+                Email = "info@dotnettutorials.net",
+                MobileNumber = "1234567890",
+                Address = "BBSR, Odisha, India"
+            };
+            //Step2: Validate the Customer
+            Validator validator = new Validator();
+            bool IsValid = validator.ValidateCustomer(customer);
+            //Step3: Save the Customer Object into the database
+            CustomerDataAccessLayer customerDataAccessLayer = new CustomerDataAccessLayer();
+            bool IsSaved = customerDataAccessLayer.SaveCustomer(customer);
+            //Step4: Send the Registration Email to the Customer
+            Email email = new Email();
+            email.SendRegistrationEmail(customer);
+            Console.ReadKey();
+        }
+    }
+
+    //public class CustomerRegistration
+    //{
+    //    public bool RegisterCustomer(Customer customer)
+    //    {
+    //        //Step1: Validate the Customer
+    //        Validator validator = new Validator();
+    //        bool IsValid = validator.ValidateCustomer(customer);
+    //        //Step1: Save the Customer Object into the database
+    //        CustomerDataAccessLayer customerDataAccessLayer = new CustomerDataAccessLayer();
+    //        bool IsSaved = customerDataAccessLayer.SaveCustomer(customer);
+    //        //Step3: Send the Registration Email to the Customer
+    //        Email email = new Email();
+    //        email.SendRegistrationEmail(customer);
+    //        return true;
+    //    }
+    //}
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        // Create an Instance of Customer Class
+    //        Customer customer = new Customer()
+    //        {
+    //            Name = "Pranaya",
+    //            Email = "info@dotnettutorials.net",
+    //            MobileNumber = "1234567890",
+    //            Address = "BBSR, Odisha, India"
+    //        };
+    //        //Using Facade Class
+    //        CustomerRegistration customerRegistration = new CustomerRegistration();
+    //        customerRegistration.RegisterCustomer(customer);
+    //        Console.ReadKey();
+    //    }
+    //}
+}
---
+++ Program1.cs
@@ -0,0 +1,145 @@
+﻿namespace AdapterDesignPattern
+{
+    //Step1
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public decimal Salary { get; set; }
+
+        public Employee(int id, string name, string designation, decimal salary)
+        {
+            ID = id;
+            Name = name;
+            Designation = designation;
+            Salary = salary;
+        }
+    }
+    //Step2
+    // The Adaptee contains some functionality that is required by the client.
+    // But this interface is not compatible with the client code.
+    public class ThirdPartyBillingSystem
+    {
+        //ThirdPartyBillingSystem accepts employee's information as a List to process each employee's salary
+        public void ProcessSalary(List<Employee> listEmployee)
+        {
+            foreach (Employee employee in listEmployee)
+            {
+                Console.WriteLine("Rs." + employee.Salary + " Salary Credited to " + employee.Name + " Account");
+            }
+        }
+    }
+    //Step3
+    // The ITarget defines the domain-specific interface used by the client code.
+    // This interface needs to be implemented by the Adapter.
+    // The client can only see this interface i.e. the class which implements the ITarget interface.
+    public interface ITarget
+    {
+        void ProcessCompanySalary(string[,] employeesArray);
+    }
+    //Step4
+    public class EmployeeAdapter : ITarget
+    {
+        //To use Object Adapter Design Pattern, we need to create an object of ThirdPartyBillingSystem
+        ThirdPartyBillingSystem thirdPartyBillingSystem = new ThirdPartyBillingSystem();
+        //The following will accept the employees in the form of string array
+        //Then convert the employee string array to List of Employees
+        //After conversation, it will call the Adaptee's Method to Process the Salaries
+        public void ProcessCompanySalary(string[,] employeesArray)
+        {
+            string Id = null;
+            string Name = null;
+            string Designation = null;
+            string Salary = null;
+            List<Employee> listEmployee = new List<Employee>();
+            for (int i = 0; i < employeesArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < employeesArray.GetLength(1); j++)
+                {
+                    if (j == 0)
+                    {
+                        Id = employeesArray[i, j];
+                    }
+                    else if (j == 1)
+                    {
+                        Name = employeesArray[i, j];
+                    }
+                    else if (j == 2)
+                    {
+                        Designation = employeesArray[i, j];
+                    }
+                    else
+                    {
+                        Salary = employeesArray[i, j];
+                    }
+                }
+                listEmployee.Add(new Employee(Convert.ToInt32(Id), Name, Designation, Convert.ToDecimal(Salary)));
+            }
+            Console.WriteLine("Adapter converted Array of Employee to List of Employee");
+            Console.WriteLine("Then delegate to the ThirdPartyBillingSystem for processing the employee salary\n");
+            thirdPartyBillingSystem.ProcessSalary(listEmployee);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Storing the Employees Data in a String Array
+            string[,] employeesArray = new string[5, 4]
+            {
+                {"101","John","SE","10000"},
+                {"102","Smith","SE","20000"},
+                {"103","Dev","SSE","30000"},
+                {"104","Pam","SE","40000"},
+                {"105","Sara","SSE","50000"}
+            };
+            //The EmployeeAdapter Makes it possible to work with Two Incompatible Interfaces
+            Console.WriteLine("HR system passes employee string array to Adapter\n");
+            ITarget target = new EmployeeAdapter();
+            target.ProcessCompanySalary(employeesArray);
+            Console.Read();
+        }
+    }
+    //public class EmployeeAdapter : ThirdPartyBillingSystem, ITarget
+    //{
+    //    //The following will accept the employees in the form of string array
+    //    //Then convert the employee string array to List of Employees
+    //    //After conversation, it will call the Adaptee's Method to Process the Salaries
+    //    public void ProcessCompanySalary(string[,] employeesArray)
+    //    {
+    //        string Id = null;
+    //        string Name = null;
+    //        string Designation = null;
+    //        string Salary = null;
+    //        List<Employee> listEmployee = new List<Employee>();
+    //        for (int i = 0; i < employeesArray.GetLength(0); i++)
+    //        {
+    //            for (int j = 0; j < employeesArray.GetLength(1); j++)
+    //            {
+    //                if (j == 0)
+    //                {
+    //                    Id = employeesArray[i, j];
+    //                }
+    //                else if (j == 1)
+    //                {
+    //                    Name = employeesArray[i, j];
+    //                }
+    //                else if (j == 2)
+    //                {
+    //                    Designation = employeesArray[i, j];
+    //                }
+    //                else
+    //                {
+    //                    Salary = employeesArray[i, j];
+    //                }
+    //            }
+    //            listEmployee.Add(new Employee(Convert.ToInt32(Id), Name, Designation, Convert.ToDecimal(Salary)));
+    //        }
+    //        Console.WriteLine("Adapter converted Array of Employee to List of Employee");
+    //        Console.WriteLine("Then delegate to the ThirdPartyBillingSystem for processing the employee salary\n");
+    //        //Call the Base Class ProcessSalary Method to Process the Salary
+    //        ProcessSalary(listEmployee);
+    //    }
+    //}
+}
